bc2114c1cbbb89c31a8d68ff800c3a54586dc94d
Piotr Ścieszka <114289131+pscieszka@users.noreply.github.com>
2023-03-28 15:12:30 +0200

    Update password.h

--- passManager/password.h
+++ passManager/password.h
@@ -1,7 +1,10 @@
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
 class password {
 public:
     std::string generate_salt(int length);
     std::string hash_password(std::string password, std::string salt);
+    std::string hash_rsa(RSA* rsa, std::string salt);
 
 };
 
